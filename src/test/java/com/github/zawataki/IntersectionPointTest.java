59ddee83111c10432045056cc042bf4771f83d3a
Yuki Takizawa <y.takizawa64@gmail.com>
2018-11-03 07:55:55 +0900

    Add getIntersectionPoint() with 'includesEndpoint' parameter
    Add getIntersectionPoint() method with 'includesEndpoint' parameter.
    If 'includesEndpoint' is true, intersection judgement includes
    a endpoint of a line. Otherwise excludes it.

--- src/test/java/com/github/zawataki/IntersectionPointTest.java
+++ src/test/java/com/github/zawataki/IntersectionPointTest.java
@@ -15,32 +15,84 @@ import static org.junit.Assert.assertThat;
 public class IntersectionPointTest {
 
     @Test
-    public void getIntersectionPoint_from_intersectedLines() {
+    public void getIntersectionPointFromIntersectedLines() {
         final Line2D line1 = new Line2D.Double(0, 0, 1, 1);
         final Line2D line2 = new Line2D.Double(1, 0, 0, 1);
 
-        final Optional<Point2D> intersectionPoint =
+        final Optional<Point2D> intersectionPointWhenExcludesEndpoint =
                 IntersectionPoint.getIntersectionPoint(line1, line2);
 
-        assertThat("Cannot get intersection point",
-                intersectionPoint.isPresent(), is(true));
+        assertThat("Cannot get intersection point when excludes endpoint",
+                intersectionPointWhenExcludesEndpoint.isPresent(), is(true));
 
         final Point2D.Double expectedIntersectionPoint =
                 new Point2D.Double(0.5, 0.5);
-        assertThat("Returned intersection point is NOT equal to " +
-                        expectedIntersectionPoint, intersectionPoint.get(),
+        assertThat("Returned intersection point when excludes endpoint, " +
+                        "is NOT equal to " + expectedIntersectionPoint,
+                intersectionPointWhenExcludesEndpoint.get(),
+                is(expectedIntersectionPoint));
+
+
+        final boolean includesEndpoint = true;
+        final Optional<Point2D> intersectionPointWhenIncludesEndpoint =
+                IntersectionPoint.getIntersectionPoint(line1, line2,
+                        includesEndpoint);
+
+        assertThat("Cannot get intersection point when includes endpoint",
+                intersectionPointWhenIncludesEndpoint.isPresent(), is(true));
+
+        assertThat("Returned intersection point when includes endpoint, " +
+                        "is NOT equal to " + expectedIntersectionPoint,
+                intersectionPointWhenIncludesEndpoint.get(),
                 is(expectedIntersectionPoint));
     }
 
     @Test
-    public void getIntersectionPoint_notIntersectedLines() {
+    public void getIntersectionPointFromNotIntersectedLines() {
         final Line2D line1 = new Line2D.Double(0, 0, 1, 1);
-        final Line2D line2 = new Line2D.Double(1, 1.1, 2, 2);
+        final Line2D line2 = new Line2D.Double(1, 1.0001, 2, 2);
 
-        final Optional<Point2D> intersectionPoint =
+        final Optional<Point2D> intersectionPointWhenExcludesEndpoint =
                 IntersectionPoint.getIntersectionPoint(line1, line2);
 
-        assertThat("Got intersection point", intersectionPoint.isPresent(),
-                is(false));
+        assertThat("Got intersection point when excludes endpoint",
+                intersectionPointWhenExcludesEndpoint.isPresent(), is(false));
+
+
+        final boolean includesEndpoint = true;
+        final Optional<Point2D> intersectionPointWhenIncludesEndpoint =
+                IntersectionPoint.getIntersectionPoint(line1, line2,
+                        includesEndpoint);
+
+        assertThat("Got intersection point when includes endpoint",
+                intersectionPointWhenIncludesEndpoint.isPresent(), is(false));
+    }
+
+    @Test
+    public void getIntersectionPointFromLinesIntersectedAtEndpoint() {
+        final Line2D line1 = new Line2D.Double(0, 0, 1, 1);
+        final Line2D line2 = new Line2D.Double(2, 0, 0, 2);
+
+        final Optional<Point2D> intersectionPointWhenExcludesEndpoint =
+                IntersectionPoint.getIntersectionPoint(line1, line2);
+
+        assertThat("Got intersection point when excludes endpoint",
+                intersectionPointWhenExcludesEndpoint.isPresent(), is(false));
+
+
+        final boolean includesEndpoint = true;
+        final Optional<Point2D> intersectionPointWhenIncludesEndpoint =
+                IntersectionPoint.getIntersectionPoint(line1, line2,
+                        includesEndpoint);
+
+        assertThat("Cannot get intersection point when includes endpoint",
+                intersectionPointWhenIncludesEndpoint.isPresent(), is(true));
+
+        final Point2D.Double expectedIntersectionPoint =
+                new Point2D.Double(1, 1);
+        assertThat("Returned intersection point when includes endpoint," +
+                        " is NOT equal to " + expectedIntersectionPoint,
+                intersectionPointWhenIncludesEndpoint.get(),
+                is(expectedIntersectionPoint));
     }
 }

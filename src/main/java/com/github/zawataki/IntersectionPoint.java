c7ddfe880e5a30f69c7b5d4f297229ebd35edad1
Yuki Takizawa <y.takizawa64@gmail.com>
2018-11-03 21:47:31 +0900

    Add getIntersectionPoints() for rectangle and line
    Fix #10
    
    Add getIntersectionPoints(Rectangle2D, Line2D, boolean) method to
    get intersection points from a rectangle and a line.

--- src/main/java/com/github/zawataki/IntersectionPoint.java
+++ src/main/java/com/github/zawataki/IntersectionPoint.java
@@ -2,9 +2,14 @@ package com.github.zawataki;
 
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * A library for getting an intersection point of lines or rectangles
@@ -30,10 +35,10 @@ public class IntersectionPoint {
     /**
      * Get intersection point from given two lines
      *
-     * @param l1               1st line
-     * @param l2               2nd line
+     * @param l1 1st line
+     * @param l2 2nd line
      * @param includesEndpoint Includes endpoint of line if {@code true}.
-     *                         Otherwise excludes.
+     * Otherwise excludes.
      *
      * @return A intersection point if exists. Otherwise empty.
      */
@@ -74,18 +79,17 @@ public class IntersectionPoint {
     /**
      * A given point is on a given line or not
      *
-     * @param point            the specified point
-     * @param line             the specified line
+     * @param point the specified point
+     * @param line the specified line
      * @param includesEndpoint Includes endpoint of line if {@code true}.
-     *                         Otherwise excludes.
+     * Otherwise excludes.
      *
      * @return {@code true} if a point is on a line. Otherwise {@code false}
      */
     static public boolean pointIsOnLine(Point2D point, Line2D line,
             boolean includesEndpoint) {
 
-        if (!includesEndpoint &&
-                (point.equals(line.getP1()) || point.equals(line.getP2()))) {
+        if (!includesEndpoint && pointIsOnLineEndpoint(point, line)) {
             return false;
         }
 
@@ -109,5 +113,78 @@ public class IntersectionPoint {
         return BigDecimal.valueOf(value)
                 .setScale(5, RoundingMode.DOWN)
                 .stripTrailingZeros();
+    }
+
+    /**
+     * Get intersection points from a given rectangle and a line.
+     *
+     * @param rect the specified rectangle
+     * @param line the specified line
+     * @param includesEndpoint If {@code true}, it's regarded as intersected in
+     * case of the followings.
+     * <ul>
+     * <li>an intersection point is on endpoint of a given line</li>
+     * <li>an intersection point is on any of vertex of given rectangle</li>
+     * </ul>
+     * If {@code false} and same situation, it's regarded as NOT intersected.
+     * But if {@code false} and the number of intersection points is greater
+     * than 1, it's regarded as intersected.
+     *
+     * @return list of intersection points if exists. Otherwise empty list.
+     */
+    static public List<Point2D> getIntersectionPoints(Rectangle2D rect,
+            Line2D line, boolean includesEndpoint) {
+
+        final List<Line2D> linesOfRectangle = getLinesOfRectangle(rect);
+
+        final List<Point2D> intersectionPoints = linesOfRectangle.stream()
+                .map(lineOfRect -> getIntersectionPoint(lineOfRect, line,
+                        true).orElse(null))
+                .filter(Objects::nonNull)
+                .distinct()
+                .collect(Collectors.toList());
+
+        if (includesEndpoint || intersectionPoints.size() != 1) {
+            return intersectionPoints;
+        }
+
+        final Point2D point = intersectionPoints.get(0);
+
+        if (pointIsOnLineEndpoint(point, line) || linesOfRectangle.stream()
+                .anyMatch(li -> pointIsOnLineEndpoint(point, li))) {
+
+            return new ArrayList<>();
+        }
+
+        return intersectionPoints;
+    }
+
+    static private List<Line2D> getLinesOfRectangle(Rectangle2D rect) {
+
+        final Point2D upperLeftPoint =
+                new Point2D.Double(rect.getX(), rect.getY());
+        final Point2D upperRightPoint =
+                new Point2D.Double(rect.getX() + rect.getWidth(), rect.getY());
+        final Point2D lowerLeftPoint =
+                new Point2D.Double(rect.getX(), rect.getY() - rect.getHeight());
+        final Point2D lowerRightPoint =
+                new Point2D.Double(rect.getX() + rect.getWidth(),
+                        rect.getY() - rect.getHeight());
+
+        List<Line2D> linesOfRect = new ArrayList<>();
+        // upper line
+        linesOfRect.add(new Line2D.Double(upperLeftPoint, upperRightPoint));
+        // lower line
+        linesOfRect.add(new Line2D.Double(lowerLeftPoint, lowerRightPoint));
+        // left line
+        linesOfRect.add(new Line2D.Double(upperLeftPoint, lowerLeftPoint));
+        // right line
+        linesOfRect.add(new Line2D.Double(upperRightPoint, lowerRightPoint));
+
+        return linesOfRect;
+    }
+
+    static private boolean pointIsOnLineEndpoint(Point2D point, Line2D line) {
+        return point.equals(line.getP1()) || point.equals(line.getP2());
     }
 }
--- src/test/java/com/github/zawataki/IntersectionPointTest.java
+++ src/test/java/com/github/zawataki/IntersectionPointTest.java
@@ -4,7 +4,13 @@ import org.junit.Test;
 
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -13,6 +19,9 @@ import static org.junit.Assert.assertThat;
  * Test for {@link IntersectionPoint}
  */
 public class IntersectionPointTest {
+
+    final private Comparator<Point2D> point2DComparator =
+            Comparator.comparing(Point2D::getX).thenComparing(Point2D::getY);
 
     @Test
     public void getIntersectionPointFromIntersectedLines() {
@@ -165,5 +174,129 @@ public class IntersectionPointTest {
     private String toStringFrom(Line2D line) {
         return String.format("Line2D[ [%s, %s] -> [%s, %s] ]", line.getX1(),
                 line.getY1(), line.getX2(), line.getY2());
+    }
+
+    @Test
+    public void getIntersectionPointsFromIntersectedRectangleAndLine() {
+        final Rectangle2D rectangle = new Rectangle2D.Double(0, 1, 1, 1);
+        final Line2D line = new Line2D.Double(-1, -1, 2, 2);
+        final boolean includesEndpoint = true;
+
+        final List<Point2D> pointsWhenIncludesEndpoint =
+                IntersectionPoint.getIntersectionPoints(rectangle, line,
+                        includesEndpoint);
+
+        final List<Point2D> expectedPoints =
+                Stream.of(new Point2D.Double(0, 0), new Point2D.Double(1, 1))
+                        .sorted(point2DComparator)
+                        .collect(Collectors.toList());
+        assertThat("Intersection points are NOT equal to " + expectedPoints +
+                " when includes endpoint", pointsWhenIncludesEndpoint.stream()
+                .sorted(point2DComparator)
+                .collect(Collectors.toList()), is(expectedPoints));
+
+        final List<Point2D> pointsWhenExcludesEndpoint =
+                IntersectionPoint.getIntersectionPoints(rectangle, line,
+                        !includesEndpoint);
+        assertThat("Intersection points are NOT equal to " + expectedPoints +
+                " when excludes endpoint", pointsWhenExcludesEndpoint.stream()
+                .sorted(point2DComparator)
+                .collect(Collectors.toList()), is(expectedPoints));
+    }
+
+    @Test
+    public void getIntersectionPointsFromNotIntersectedRectangleAndLine() {
+        final Rectangle2D rectangle = new Rectangle2D.Double(0, 1, 1, 1);
+        final Line2D line = new Line2D.Double(1, 1.00001, 2, 2);
+        final boolean includesEndpoint = true;
+
+        final List<Point2D> pointsWhenIncludesEndpoint =
+                IntersectionPoint.getIntersectionPoints(rectangle, line,
+                        includesEndpoint);
+
+        assertThat("Intersection points NOT found when includes endpoint",
+                pointsWhenIncludesEndpoint.isEmpty(), is(true));
+
+        final List<Point2D> pointsWhenExcludesEndpoint =
+                IntersectionPoint.getIntersectionPoints(rectangle, line,
+                        !includesEndpoint);
+        assertThat("Intersection points NOT found when excludes endpoint",
+                pointsWhenExcludesEndpoint.isEmpty(), is(true));
+    }
+
+    @Test
+    public void getIntersectionPointsFromRectangleAndLineIntersectedAtEndpoint() {
+        final Rectangle2D rectangle = new Rectangle2D.Double(0, 1, 1, 1);
+        final Line2D line = new Line2D.Double(1, 1, 2, 0);
+        final boolean includesEndpoint = true;
+
+        final List<Point2D> pointsWhenIncludesEndpoint =
+                IntersectionPoint.getIntersectionPoints(rectangle, line,
+                        includesEndpoint);
+
+        final List<Point2D> expectedPoints =
+                Arrays.asList(new Point2D.Double(1, 1));
+        assertThat("Intersection points are NOT equal to " + expectedPoints +
+                " when includes endpoint", pointsWhenIncludesEndpoint.stream()
+                .sorted(point2DComparator)
+                .collect(Collectors.toList()), is(expectedPoints));
+
+        final List<Point2D> pointsWhenExcludesEndpoint =
+                IntersectionPoint.getIntersectionPoints(rectangle, line,
+                        !includesEndpoint);
+        assertThat("Intersection points NOT found when excludes endpoint",
+                pointsWhenExcludesEndpoint.isEmpty(), is(true));
+    }
+
+    @Test
+    public void getIntersectionPointsFromRectangleAndLineIntersectedAtVertex() {
+        final Rectangle2D rectangle = new Rectangle2D.Double(0, 1, 1, 1);
+        final Line2D line = new Line2D.Double(0, 2, 2, 0);
+        final boolean includesEndpoint = true;
+
+        final List<Point2D> pointsWhenIncludesEndpoint =
+                IntersectionPoint.getIntersectionPoints(rectangle, line,
+                        includesEndpoint);
+
+        final List<Point2D> expectedPoints =
+                Arrays.asList(new Point2D.Double(1, 1));
+        assertThat("Intersection points are NOT equal to " + expectedPoints +
+                " when includes endpoint", pointsWhenIncludesEndpoint.stream()
+                .sorted(point2DComparator)
+                .collect(Collectors.toList()), is(expectedPoints));
+
+        final List<Point2D> pointsWhenExcludesEndpoint =
+                IntersectionPoint.getIntersectionPoints(rectangle, line,
+                        !includesEndpoint);
+        assertThat("Intersection points NOT found when excludes endpoint",
+                pointsWhenExcludesEndpoint.isEmpty(), is(true));
+    }
+
+    @Test
+    public void getIntersectionPointsFromRectangleAndLineIntersectedAtBothEndpoint() {
+        final Rectangle2D rectangle = new Rectangle2D.Double(0, 1, 1, 1);
+        final Line2D line = new Line2D.Double(0.0, 0.5, 1, 0.5);
+        final boolean includesEndpoint = true;
+
+        final List<Point2D> pointsWhenIncludesEndpoint =
+                IntersectionPoint.getIntersectionPoints(rectangle, line,
+                        includesEndpoint);
+
+        final List<Point2D> expectedPoints =
+                Stream.of(line.getP1(), line.getP2())
+                        .sorted(point2DComparator)
+                        .collect(Collectors.toList());
+        assertThat("Intersection points are NOT equal to " + expectedPoints +
+                " when includes endpoint", pointsWhenIncludesEndpoint.stream()
+                .sorted(point2DComparator)
+                .collect(Collectors.toList()), is(expectedPoints));
+
+        final List<Point2D> pointsWhenExcludesEndpoint =
+                IntersectionPoint.getIntersectionPoints(rectangle, line,
+                        !includesEndpoint);
+        assertThat("Intersection points are NOT equal to " + expectedPoints +
+                " when excludes endpoint", pointsWhenExcludesEndpoint.stream()
+                .sorted(point2DComparator)
+                .collect(Collectors.toList()), is(expectedPoints));
     }
 }
